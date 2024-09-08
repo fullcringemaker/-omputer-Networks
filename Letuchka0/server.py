29ea77a1b8caa12df008bc8723302ae865e3d61d
fullcringemaker <dmitriytrofimenko105@mail.ru>
2024-09-08 20:12:59 +0300

    Create server.py

---
+++ Letuchka0/server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import asyncio
+import websockets
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print("client say: ")
+
+    while(True):
+        greeting = input("enter answer to client: ")
+        await websocket.send(greeting)
+        print(str(greeting))
+
+start_server = websockets.serve(hello, "185.104.251.226", 9742)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
