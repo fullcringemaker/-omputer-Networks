1f9e99d393726cb18504242d83e08ec5c17d8201
fullcringemaker <dmitriytrofimenko105@mail.ru>
2024-11-04 03:24:09 +0300

    Update ssh-server.go

--- Lab4.1,4.2/ssh-server.go
+++ Lab4.1,4.2/ssh-server.go
@@ -22,40 +22,38 @@ func main() {
 					if err == io.EOF {
 						break
 					}
-					io.WriteString(s, "Ошибка чтения команды: "+err.Error()+"\n")
+					io.WriteString(s, "Error reading command: "+err.Error()+"\n")
 					continue
 				}
-				
 				args := strings.Fields(line)
 				if len(args) == 0 {
 					continue
 				}
-
 				switch args[0] {
 				case "exit", "quit":
-					io.WriteString(s, "Выход из сессии.\n")
+					io.WriteString(s, "Exit session.\n")
 					return
 				case "mkdir":
 					if len(args) < 2 {
-						io.WriteString(s, "Использование: mkdir <путь>\n")
+						io.WriteString(s, "Usage: mkdir <path>\n")
 						continue
 					}
 					err := os.MkdirAll(args[1], 0755)
 					if err != nil {
-						io.WriteString(s, "Ошибка создания директории: "+err.Error()+"\n")
+						io.WriteString(s, "Error creating directory: "+err.Error()+"\n")
 					} else {
-						io.WriteString(s, "Директория создана.\n")
+						io.WriteString(s, "Directory created.\n")
 					}
 				case "rmdir":
 					if len(args) < 2 {
-						io.WriteString(s, "Использование: rmdir <путь>\n")
+						io.WriteString(s, "Usage: rmdir <path>\n")
 						continue
 					}
 					err := os.RemoveAll(args[1])
 					if err != nil {
-						io.WriteString(s, "Ошибка удаления директории: "+err.Error()+"\n")
+						io.WriteString(s, "Error deleting directory: "+err.Error()+"\n")
 					} else {
-						io.WriteString(s, "Директория удалена.\n")
+						io.WriteString(s, "Directory deleted.\n")
 					}
 				case "ls":
 					path := "."
@@ -64,7 +62,7 @@ func main() {
 					}
 					files, err := os.ReadDir(path)
 					if err != nil {
-						io.WriteString(s, "Ошибка чтения директории: "+err.Error()+"\n")
+						io.WriteString(s, "Error reading directory: "+err.Error()+"\n")
 						continue
 					}
 					for _, file := range files {
@@ -72,59 +70,59 @@ func main() {
 					}
 				case "mv":
 					if len(args) < 3 {
-						io.WriteString(s, "Использование: mv <источник> <назначение>\n")
+						io.WriteString(s, "Usage: mv <source> <destination>\n")
 						continue
 					}
 					err := os.Rename(args[1], args[2])
 					if err != nil {
-						io.WriteString(s, "Ошибка перемещения файла: "+err.Error()+"\n")
+						io.WriteString(s, "Error moving file: "+err.Error()+"\n")
 					} else {
-						io.WriteString(s, "Файл перемещен.\n")
+						io.WriteString(s, "The file has been moved.\n")
 					}
 				case "rm":
 					if len(args) < 2 {
-						io.WriteString(s, "Использование: rm <имя файла>\n")
+						io.WriteString(s, "Usage: rm <filename>\n")
 						continue
 					}
 					err := os.Remove(args[1])
 					if err != nil {
-						io.WriteString(s, "Ошибка удаления файла: "+err.Error()+"\n")
+						io.WriteString(s, "Error deleting file: "+err.Error()+"\n")
 					} else {
-						io.WriteString(s, "Файл удален.\n")
+						io.WriteString(s, "The file has been deleted.\n")
 					}
 				case "ping":
 					if len(args) < 2 {
-						io.WriteString(s, "Использование: ping <адрес>\n")
+						io.WriteString(s, "Usage: ping <address>\n")
 						continue
 					}
 					cmd := exec.Command("ping", "-c", "4", args[1])
 					output, err := cmd.CombinedOutput()
 					if err != nil {
-						io.WriteString(s, "Ошибка выполнения ping: "+err.Error()+"\n")
+						io.WriteString(s, "Error executing ping: "+err.Error()+"\n")
 					}
 					io.WriteString(s, string(output))
 				case "touch":
 					if len(args) < 2 {
-						io.WriteString(s, "Использование: touch <путь к файлу>\n")
+						io.WriteString(s, "Usage: touch <file path>\n")
 						continue
 					}
 					file, err := os.Create(args[1])
 					if err != nil {
-						io.WriteString(s, "Ошибка создания файла: "+err.Error()+"\n")
+						io.WriteString(s, "Error creating file: "+err.Error()+"\n")
 						continue
 					}
 					file.Close()
-					io.WriteString(s, "Файл создан.\n")
+					io.WriteString(s, "The file has been created.\n")
 				default:
-					io.WriteString(s, "Неизвестная команда.\n")
+					io.WriteString(s, "Unknown command.\n")
 				}
 			}
 		},
 		PasswordHandler: func(ctx ssh.Context, password string) bool {
-			log.Printf("Попытка авторизации: пользователь=%s пароль=%s\n", ctx.User(), password)
+			log.Printf("Authorization attempt: user=%s password=%s\n", ctx.User(), password)
 			return ctx.User() == "testuser" && password == "password123"
 		},
 	}
-	log.Println("Запуск SSH-сервера на порту 9742...")
+	log.Println("Running an SSH-server on port 9742...")
 	log.Fatal(server.ListenAndServe())
 }
