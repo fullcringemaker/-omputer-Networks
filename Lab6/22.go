064011ce4c44e489fad8fd02aa6f5ea6820ae424
fullcringemaker <dmitriytrofimenko105@mail.ru>
2024-11-08 06:07:58 +0300

    Update 22.go

--- Lab6/22.go
+++ Lab6/22.go
@@ -1,9 +1,35 @@
-{{define "newsItem"}}
-<div>
-    <h2>{{.Title}}</h2>
-    <p><em>{{.Date}}</em></p>
-    <p>{{.Content}}</p>
-    <p><a href="{{.Link}}">Read more</a></p>
-    <hr>
-</div>
-{{end}}
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Новости</title>
+    <script>
+        var ws = new WebSocket("ws://" + window.location.host + "/ws");
+        ws.onmessage = function(event) {
+            var newsItems = JSON.parse(event.data);
+            var content = "";
+            newsItems.forEach(function(item) {
+                content += `
+                    <div class="news-item">
+                        <h2><a href="${item.Link}" target="_blank">${item.Title}</a></h2>
+                        <p>${item.Description}</p>
+                        <small>${new Date(item.PubDate).toLocaleString('ru-RU')}</small>
+                        <hr>
+                    </div>
+                `;
+            });
+            document.getElementById("content").innerHTML = content;
+        };
+        ws.onclose = function() {
+            console.log("Соединение закрыто, повторная попытка...");
+            setTimeout(function() {
+                location.reload();
+            }, 5000);
+        };
+    </script>
+</head>
+<body>
+    <h1>Новости</h1>
+    <div id="content"></div>
+</body>
+</html>
