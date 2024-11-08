430955e90aee276c92ef5314a11ce6a261b15245
fullcringemaker <dmitriytrofimenko105@mail.ru>
2024-11-08 05:45:04 +0300

    Update 22.go

--- Lab6/22.go
+++ Lab6/22.go
@@ -1,48 +1,9 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Новости</title>
-</head>
-<body>
-    <div id="news-container">
-        {{template "parser.html" .}}
-    </div>
-
-    <script>
-        var ws = new WebSocket("ws://" + location.host + "/ws");
-
-        ws.onmessage = function(event) {
-            var newsItems = JSON.parse(event.data);
-            var container = document.getElementById("news-container");
-            container.innerHTML = "";
-
-            newsItems.forEach(function(item) {
-                var div = document.createElement("div");
-                div.className = "news-item";
-
-                var title = document.createElement("h2");
-                var titleLink = document.createElement("a");
-                titleLink.href = item.Link;
-                titleLink.target = "_blank";
-                titleLink.textContent = item.Title;
-                title.appendChild(titleLink);
-                div.appendChild(title);
-
-                var description = document.createElement("p");
-                description.textContent = item.Description;
-                div.appendChild(description);
-
-                var date = document.createElement("small");
-                date.textContent = item.Date;
-                div.appendChild(date);
-
-                var hr = document.createElement("hr");
-                div.appendChild(hr);
-
-                container.appendChild(div);
-            });
-        };
-    </script>
-</body>
-</html>
+{{define "newsItem"}}
+<div>
+    <h2>{{.Title}}</h2>
+    <p><em>{{.Date}}</em></p>
+    <p>{{.Content}}</p>
+    <p><a href="{{.Link}}">Read more</a></p>
+    <hr>
+</div>
+{{end}}
