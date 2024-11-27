29237e82400092d57de6144420d5381ef9a04d47
fullcringemaker <dmitriytrofimenko105@mail.ru>
2024-11-27 13:48:06 +0300

    Update ftpclient.go

--- Lab6/Lab6.1/ftpclient.go
+++ Lab6/Lab6.1/ftpclient.go
@@ -1,192 +1,203 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
+	"html/template"
 	"log"
-	"os"
+	"net/http"
 	"path"
 	"strings"
 	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/jlaffaye/ftp"
 )
 
-const (
-	ftpHost = "students.yss.su:21"
-	ftpUser = "ftpiu8"
-	ftpPass = "3Ru7yOTA"
+type FtpSession struct {
+	Client *ftp.ServerConn
+}
+
+var (
+	upgrader   = websocket.Upgrader{}
+	sessions   = make(map[string]*FtpSession)
+	templates  = template.Must(template.ParseFiles("index.html", "work.html"))
+	serverIP   = "185.104.251.226"
+	serverPort = 9742
 )
 
 func main() {
-	c, err := ftp.Dial(ftpHost, ftp.DialWithTimeout(5*time.Second))
-	if err != nil {
-		log.Fatalf("Failed to connect to FTP server: %v", err)
-	}
-	defer c.Quit()
-	err = c.Login(ftpUser, ftpPass)
-	if err != nil {
-		log.Fatalf("Failed to login: %v", err)
-	}
-	fmt.Println("Connection and authorization to the FTP server completed")
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("ftp> ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
-		if input == "" {
-			continue
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/work", workHandler)
+	http.HandleFunc("/ws", wsHandler)
+	addr := fmt.Sprintf("%s:%d", serverIP, serverPort)
+	fmt.Printf("The server is running on http://%s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		templates.ExecuteTemplate(w, "index.html", nil)
+	} else if r.Method == http.MethodPost {
+		ftpHost := r.FormValue("ftpHost")
+		ftpUser := r.FormValue("ftpUser")
+		ftpPass := r.FormValue("ftpPass")
+		if !strings.Contains(ftpHost, ":") {
+			ftpHost = ftpHost + ":21"
 		}
-		args := strings.Split(input, " ")
-		cmd := strings.ToLower(args[0])
-		switch cmd {
-		case "upload":
-			if len(args) != 2 {
-				fmt.Println("Usage: upload <local_path>")
-				continue
-			}
-			uploadFile(c, args[1])
-		case "download":
-			if len(args) != 2 {
-				fmt.Println("Usage: download <remote_path>")
-				continue
-			}
-			downloadFile(c, args[1])
-		case "mkdir":
-			if len(args) != 2 {
-				fmt.Println("Usage: mkdir <directory_name>")
-				continue
-			}
-			makeDir(c, args[1])
-		case "delete":
-			if len(args) != 2 {
-				fmt.Println("Usage: delete <remote_file>")
-				continue
-			}
-			deleteFile(c, args[1])
-		case "ls":
-			listDir(c)
-		case "cd":
-			if len(args) != 2 {
-				fmt.Println("Usage: cd <directory>")
-				continue
-			}
-			changeDir(c, args[1])
-		case "rmdir":
-			if len(args) != 2 {
-				fmt.Println("Usage: rmdir <directory>")
-				continue
-			}
-			removeDir(c, args[1], false)
-		case "rmr":
-			if len(args) != 2 {
-				fmt.Println("Usage: rmr <directory>")
-				continue
-			}
-			err := removeDirRecursively(c, args[1])
-			if err != nil {
-				fmt.Printf("Error recursively deleting directory: %v\n", err)
-			} else {
-				fmt.Println("Directory was deleted recursively")
-			}
-		case "quit", "exit":
-			fmt.Println("Exit FTP client")
+		c, err := ftp.Dial(ftpHost, ftp.DialWithTimeout(5*time.Second))
+		if err != nil {
+			http.Error(w, "Failed to connect to FTP server: "+err.Error(), http.StatusInternalServerError)
 			return
-		default:
-			fmt.Println("Unknown command. Available commands: upload, download, mkdir, delete, ls, cd, rmdir, rmr, quit")
+		}
+		err = c.Login(ftpUser, ftpPass)
+		if err != nil {
+			http.Error(w, "Failed to login: "+err.Error(), http.StatusUnauthorized)
+			return
+		}
+		sessionID := fmt.Sprintf("%s-%d", strings.ReplaceAll(r.RemoteAddr, ":", "-"), time.Now().UnixNano())
+		sessions[sessionID] = &FtpSession{Client: c}
+		http.Redirect(w, r, "/work?session="+sessionID, http.StatusFound)
+	}
+}
+func workHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "work.html", nil)
+}
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Error upgrading to WebSocket:", err)
+		return
+	}
+	defer ws.Close()
+	sessionID := r.URL.Query().Get("session")
+	if sessionID == "" {
+		ws.WriteMessage(websocket.TextMessage, []byte("Session ID is missing"))
+		return
+	}
+	session, ok := sessions[sessionID]
+	if !ok {
+		ws.WriteMessage(websocket.TextMessage, []byte("Invalid session ID"))
+		return
+	}
+	defer session.Client.Quit()
+	delete(sessions, sessionID)
+	for {
+		_, message, err := ws.ReadMessage()
+		if err != nil {
+			log.Println("Error reading message:", err)
+			break
+		}
+		cmd := string(message)
+		response := handleCommand(session.Client, cmd)
+		err = ws.WriteMessage(websocket.TextMessage, []byte(response))
+		if err != nil {
+			log.Println("Error sending message:", err)
+			break
 		}
 	}
 }
-func uploadFile(c *ftp.ServerConn, localPath string) {
-	file, err := os.Open(localPath)
-	if err != nil {
-		fmt.Printf("Error opening local file: %v\n", err)
-		return
+func handleCommand(c *ftp.ServerConn, cmd string) string {
+	args := parseCommand(cmd)
+	if len(args) == 0 {
+		return "Command not entered"
 	}
-	defer file.Close()
-	remotePath := path.Base(localPath)
-	err = c.Stor(remotePath, file)
-	if err != nil {
-		fmt.Printf("File download error: %v\n", err)
-		return
+	switch args[0] {
+	case "ls":
+		return listDir(c)
+	case "cd":
+		if len(args) < 2 {
+			return "Usage: cd <directory>"
+		}
+		return changeDir(c, args[1])
+	case "upload":
+		return "Loading via web interface is not implemented"
+	case "download":
+		return "Downloading via web interface is not implemented"
+	case "mkdir":
+		if len(args) < 2 {
+			return "Usage: mkdir <directory_name>"
+		}
+		return makeDir(c, args[1])
+	case "delete":
+		if len(args) < 2 {
+			return "Usage: delete <file_name>"
+		}
+		return deleteFile(c, args[1])
+	case "rmdir":
+		if len(args) < 2 {
+			return "Usage: rmdir <directory>"
+		}
+		return removeDir(c, args[1], false)
+	case "rmr":
+		if len(args) < 2 {
+			return "Usage: rmr <directory>"
+		}
+		err := removeDirRecursively(c, args[1])
+		if err != nil {
+			return "Error recursively deleting directory: " + err.Error()
+		}
+		return "Directory was deleted recursively"
+	default:
+		return "Unknown command. Available commands: upload, download, mkdir, delete, ls, cd, rmdir, rmr, quit"
 	}
-	fmt.Println("File was uploaded")
 }
-func downloadFile(c *ftp.ServerConn, remotePath string) {
-	r, err := c.Retr(remotePath)
-	if err != nil {
-		fmt.Printf("Error downloading file: %v\n", err)
-		return
-	}
-	defer r.Close()
-	localPath := path.Base(remotePath)
-	file, err := os.Create(localPath)
-	if err != nil {
-		fmt.Printf("Error creating local file: %v\n", err)
-		return
-	}
-	defer file.Close()
-	_, err = io.Copy(file, r)
-	if err != nil {
-		fmt.Printf("Error writing to local file: %v\n", err)
-		return
-	}
-	fmt.Println("File was downloaded")
+func parseCommand(input string) []string {
+	input = strings.TrimSpace(input)
+	return strings.Fields(input)
 }
-func makeDir(c *ftp.ServerConn, dirName string) {
-	err := c.MakeDir(dirName)
-	if err != nil {
-		fmt.Printf("Error creating directory: %v\n", err)
-		return
-	}
-	fmt.Println("Directory was created")
-}
-func deleteFile(c *ftp.ServerConn, fileName string) {
-	err := c.Delete(fileName)
-	if err != nil {
-		fmt.Printf("Error deleting file: %v\n", err)
-		return
-	}
-	fmt.Println("File was deleted")
-}
-func listDir(c *ftp.ServerConn) {
+func listDir(c *ftp.ServerConn) string {
 	entries, err := c.List("")
 	if err != nil {
-		fmt.Printf("Error getting directory listing: %v\n", err)
-		return
+		return "Error getting directory listing: " + err.Error()
 	}
+	var result strings.Builder
 	for _, entry := range entries {
-		fmt.Printf("%s\t%s\t%d\n", entry.Type, entry.Name, entry.Size)
+		var typeIndicator string
+		if entry.Type == ftp.EntryTypeFolder {
+			typeIndicator = "[D]"
+		} else {
+			typeIndicator = "[F]"
+		}
+		result.WriteString(fmt.Sprintf("%s\t%s\n", typeIndicator, entry.Name))
 	}
+	return result.String()
 }
-func changeDir(c *ftp.ServerConn, dir string) {
+func changeDir(c *ftp.ServerConn, dir string) string {
 	err := c.ChangeDir(dir)
 	if err != nil {
-		fmt.Printf("Error changing directory: %v\n", err)
-		return
+		return "Error changing directory: " + err.Error()
 	}
-	fmt.Println("Current directory was changed")
+	return "Current directory was changed"
 }
-func removeDir(c *ftp.ServerConn, dir string, recursive bool) {
+func makeDir(c *ftp.ServerConn, dirName string) string {
+	err := c.MakeDir(dirName)
+	if err != nil {
+		return "Error creating directory: " + err.Error()
+	}
+	return "Directory was created"
+}
+func deleteFile(c *ftp.ServerConn, fileName string) string {
+	err := c.Delete(fileName)
+	if err != nil {
+		return "Error deleting file: " + err.Error()
+	}
+	return "File was deleted "
+}
+func removeDir(c *ftp.ServerConn, dir string, recursive bool) string {
 	if recursive {
 		err := removeDirRecursively(c, dir)
 		if err != nil {
-			fmt.Printf("Error recursively deleting directory: %v\n", err)
-			return
+			return "Error recursively deleting directory: " + err.Error()
 		}
-		fmt.Println("Directory was deleted recursively")
+		return "Directory was deleted recursively"
 	} else {
 		err := c.RemoveDir(dir)
 		if err != nil {
-			fmt.Printf("Error deleting directory: %v\n", err)
-			return
+			return "Error deleting directory: " + err.Error()
 		}
-		fmt.Println("Directory was deleted")
+		return "Directory was deleted"
 	}
 }
 func removeDirRecursively(c *ftp.ServerConn, dir string) error {
-	fmt.Printf("Start of recursive directory deletion: %s\n", dir)
 	entries, err := c.List(dir)
 	if err != nil {
 		return err
@@ -201,19 +212,16 @@ func removeDirRecursively(c *ftp.ServerConn, dir string) error {
 			if err != nil {
 				return err
 			}
-			fmt.Printf("Directory was deleted: %s\n", fullPath)
 		} else {
 			err = c.Delete(fullPath)
 			if err != nil {
 				return err
 			}
-			fmt.Printf("File was deleted: %s\n", fullPath)
 		}
 	}
 	err = c.RemoveDir(dir)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("Directory %s was deleted recursively\n", dir)
 	return nil
 }
